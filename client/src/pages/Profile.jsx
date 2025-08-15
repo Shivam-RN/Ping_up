2fba72144171570fab18c5fa0e3ce102fc84e104
Shivam-RN <shivamrana1809@gmail.com>
2025-08-15 20:44:05 +0530

    create edit profile component

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,6 +6,7 @@ import Loading from '../components/Loading'
 import UserProfileInfo from '../components/UserProfileInfo'
 import PostCard from '../components/PostCard'
 import moment from 'moment'
+import ProfileModal from '../components/ProfileModal'
 
 
 const Profile = () => {
@@ -75,7 +76,7 @@ const Profile = () => {
         </div>
       </div>
       {/* Edit Profile Modal */}
-      {showEdit && <p>show edit profile</p>}
+      {showEdit && <ProfileModal setShowEdit={setShowEdit}/>}
     </div>
   ) : (<Loading />)
 }
