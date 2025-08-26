62d825e43f11de4ae5f4c847b4d27e4408039ef0
Shivam-RN <shivamrana1809@gmail.com>
2025-08-26 20:14:02 +0530

    create connection funcitons and integrate nodemailer

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js"
 import fs from 'fs'
 import imagekit from "../configs/imageKit.js";
+import Connection from "../models/Connection.js";
 
 // Get User Data using userId
 export const getUserData = async (req, res) => {
@@ -163,6 +164,116 @@ export const unfollowUser = async (req, res) => {
         res.json({success: false, message: error.message})
     }
 }
+
+// Send Connection Request
+export const sendConnectionRequest = async (req, res) => {
+    try {
+        const {userId} = req.auth()
+        const { id } = req.body;
+
+        const last24Hours = new Date(Date.now() - 24 * 60 * 60 * 1000)
+
+        const connectionRequests = await Connection.find({from_user_id: userId, created_at: { $gt: last24Hours }})
+        if(connectionRequests.length >= 20){
+            return res.json({success: false, message: 'You have sent more than 20 connection requests in the last 24 hours'})
+        }
+
+        const connection = await Connection.findOne({
+            $or: [
+                {from_user_id: userId, to_user_id: id},
+                {from_user_id: id, to_user_id: userId},
+            ]
+        })
+
+        if(!connection){
+           await Connection.create({
+                from_user_id: userId,
+                to_user_id: id
+            })
+
+            return res.json({success: true, message: 'Connection request sent successfully'})
+
+        }else if(connection && connection.status === 'accepted'){
+            return res.json({success: false, message: 'You are already connected with this user'})
+        }
+
+        return res.json({success: false, message: 'Connection request pending'})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
+}
+
+// Get User Connections
+export const getUserConnections = async (req, res) => {
+    try {
+        const {userId} = req.auth()
+        const user = await User.findById(userId).populate('connections followers following')
+
+        const connections = user.connections
+        const followers = user.followers
+        const following = user.following
+
+        const pendingConnections = (await Connection.find({to_user_id: userId, status: 'pending'}).populate('from_user_id')).map(connection=>connection.from_user_id)
+
+        res.json({success: true, connections, followers, following, pendingConnections})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
+}
+
+// Accept Connection Request
+export const acceptConnectionRequest = async (req, res) => {
+    try {
+        const {userId} = req.auth()
+        const { id } = req.body;
+
+        const connection = await Connection.findOne({from_user_id: id, to_user_id: userId})
+
+        if(!connection){
+            return res.json({ success: false, message: 'Connection not found' });
+        }
+
+        const user = await User.findById(userId);
+        user.connections.push(id);
+        await user.save()
+
+        const toUser = await User.findById(id);
+        toUser.connections.push(userId);
+        await toUser.save()
+
+        connection.status = 'accepted';
+        await connection.save()
+
+        res.json({ success: true, message: 'Connection accepted successfully' });
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
+}
+
+
+// Get User Profiles
+// export const getUserProfiles = async (req, res) =>{
+//     try {
+//         const { profileId } = req.body;
+//         const profile = await User.findById(profileId)
+//         if(!profile){
+//             return res.json({ success: false, message: "Profile not found" });
+//         }
+//         const posts = await Post.find({user: profileId}).populate('user')
+
+//         res.json({success: true, profile, posts})
+//     } catch (error) {
+//         console.log(error);
+//         res.json({success: false, message: error.message})
+//     }
+// }
+
 
 
 
