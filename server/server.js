e4606487d681d8ffe5eec1a4f07217761b5d8cb8
Shivam-RN <shivamrana1809@gmail.com>
2025-08-27 20:11:25 +0530

    create Story and Message models, controller functions and routes

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ import { serve } from "inngest/express";
 import { clerkMiddleware } from '@clerk/express'
 import userRouter from './routes/userRoutes.js';
 import postRouter from './routes/postRoutes.js';
+import storyRouter from './routes/storyRoutes.js';
+import messageRouter from './routes/messageRoutes.js';
 
 const app = express()
 
@@ -17,12 +19,14 @@ app.use(cors());
 app.use(clerkMiddleware())
 
 app.get('/', (req, res) => {
-    res.send('Server is live');
+    res.send('Server is live'); 
 })
 
 app.use('/api/inngest', serve({client: inngest, functions}));
 app.use('/api/user', userRouter);
 app.use('/api/post', postRouter);
+app.use('/api/story', storyRouter)
+app.use('/api/message', messageRouter)
 
 const PORT = 3000;
 
