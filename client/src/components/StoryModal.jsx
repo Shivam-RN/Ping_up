87c1051de96085f0fff9bf3c2870ca86e48150d1
Shivam-RN <shivamrana1809@gmail.com>
2025-09-01 20:55:02 +0530

    update storyModal and StoriesBar

--- client/src/components/StoryModal.jsx
+++ client/src/components/StoryModal.jsx
@@ -1,6 +1,9 @@
 import { ArrowLeft, Sparkle, TextIcon, Upload } from "lucide-react";
 import React from "react";
 import { useState } from "react";
+import { useAuth } from "@clerk/clerk-react";
+import api from "../api/axios";
+import toast from "react-hot-toast";
 
 const StoryModal = ({ setShowModal, fetchStories }) => {
     const bgColors = [
@@ -18,15 +21,76 @@ const StoryModal = ({ setShowModal, fetchStories }) => {
     const [media, setMedia] = useState(null);
     const [previewUrl, setPreviewUrl] = useState(null);
 
+    const {getToken} = useAuth();
+
+    const MAX_VIDEO_DURATION = 60
+    const MAX_VIDEO_SIZE = 50 
+
     const handleMediaUpload = (e) => {
         const file = e.target.files?.[0];
         if (file) {
-            setMedia(file);
-            setPreviewUrl(URL.createObjectURL(file));
-        }
-    };
+            if(file.type.startsWith('video')){
+                if(file.size > MAX_VIDEO_SIZE * 1024 * 1024){
+                    toast.error(`Video size should be less than ${MAX_VIDEO_SIZE}MB`);
+                    setMedia(null);
+                    setPreviewUrl(null);
+                    return;
+                }
 
-    const handleCreateStory = async () => { };
+            const video = document.createElement('video')
+            video.preload = 'metadata'
+            video.onloadedmetadata = ()=>{
+                window.URL.revokeObjectURL(video.src)
+                if(video.duration > MAX_VIDEO_DURATION){
+                    toast.error("Video duration cannot exceed 1 minute")
+                    setMedia(null)
+                    setPreviewUrl(null)
+                }else{
+                    setMedia(file)
+                    setPreviewUrl(URL.createObjectURL(file))
+                    setText('')
+                    setMode("media")
+                }
+            }
+
+            video.src = URL.createObjectURL(file)
+           }else if(file.type.startsWith("image")){
+                setMedia(file)
+                setPreviewUrl(URL.createObjectURL(file))
+                setText('')
+                setMode("media")
+           }
+        };
+    }
+
+    const handleCreateStory = async () => {
+        const media_type = mode === 'media' ? media?.type.startsWith('image') ? 'image' : 'video' : 'text';;
+
+        if(media_type === "text" && !text){
+            toast.error("Please enter some text");  
+        }
+
+        let formData = new FormData();
+        formData.append("content", text);
+        formData.append("media_type", media_type); 
+        formData.append("media", media); 
+        formData.append("background_color", background);
+
+        const token = await getToken();
+        try {
+            const {data} = await api.post('/api/story/create', formData, { headers: { Authorization: `Bearer ${token}` } });
+            if(data.success){
+                setShowModal(false);
+                toast.success("Story created successfully");
+                fetchStories();
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+    
 
     return (
         <div className="fixed inset-0 z-110 min-h-screen bg-black/80 backdrop-blur text-white flex items-center justify-center p-4" >
